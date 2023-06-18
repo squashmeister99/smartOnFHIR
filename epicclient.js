d32393eb8e8460eba8bdba77e7d69b65ac584732
Rajesh Vaidya <Rajesh.Vaidya@enghouse.com>
2023-06-17 23:26:08 -0700

    auth code implementation. not quite working yet

--- epicclient.js
+++ epicclient.js
@@ -75,7 +75,107 @@ async function getEndpoints(apiBaseUrl, epicClientId) {
   }
 
 
+  async function getAuthorizationCode(
+    authorizeUri,
+    epicClientId,
+    launchToken,
+    aud,
+    redirectUri,
+    scope,
+    state,
+    pkce = null
+  ) {
+    console.log(
+      'Requesting authorization code from EPIC, url: %s',
+      authorizeUri
+    );
+  
+    let queryParams = new URLSearchParams();
+    queryParams.append('response_type', 'code');
+    queryParams.append('client_id', epicClientId);
+    queryParams.append('redirect_uri', encodeURIComponent(redirectUri));
+    queryParams.append('state', state);
+    queryParams.append('scope', scope);
+    queryParams.append('launch', launchToken);
+    queryParams.append('aud', encodeURIComponent(aud));
+    if (pkce) {
+      queryParams.append('code_challenge', pkce.code_challenge);
+      queryParams.append('code_challenge_method', pkce.code_challenge_method);
+    }
+  
 
+    let rawLocation;
+    try {
+      let response = await got(authorizeUri, {
+        https: {
+          rejectUnauthorized: false,
+        },
+        timeout: 1000,
+        retry: {
+          limit: 10,
+        },
+        followRedirect: false,
+        searchParams: queryParams,
+        headers: {
+          'User-Agent': userAgent,
+        },
+      });
+
+      //console.log(response);
+
+      /* Extract response parameters from the redirect_uri query string.
+       */
+      rawLocation = response.headers['location'];
+      console.log('getAuthorizationCode: location header: %s', rawLocation);
+      const locationUrl = new URL(rawLocation); /* Can throw a TypeError */
+      if (`${locationUrl.origin}${locationUrl.pathname}` === redirectUri) {
+        if (locationUrl.searchParams.get('state') === state) {
+          const code = locationUrl.searchParams.get('code');
+          const error = locationUrl.searchParams.get('error');
+          if (code && !error) {
+            return code;
+          } else {
+            console.log(
+              'EPIC authorization code request failed: received error: "%s"',
+              error
+            );
+          }
+        } else {
+          console.log(
+            'EPIC authorization code request failed: invalid state parameter, expected: "%s", got: "%s"',
+            state,
+            locationUrl.searchParams.get('state')
+          );
+        }
+      } else {
+        console.log(
+          'EPIC authorization code request failed: invalid redirect uri, expected: "%s", got: "%s"',
+          redirectUri,
+          `${locationUrl.origin}${locationUrl.pathname}`
+        );
+      }
+    } catch (error) {
+
+      let reason;
+      if (error.name === 'RequestError') {
+        reason = 'Unable to connect to EPIC';
+      } else if (error.name === 'HTTPError') {
+        reason = error?.message;
+      } else if (error.name === 'TypeError') {
+        reason = rawLocation
+          ? 'Invalid location header'
+          : 'Missing location header';
+      } else {
+        reason = error.name;
+      }
+      console.log(
+        'EPIC authorization code request failed: %s - %s',
+        authorizeUri,
+        reason
+      );
+    }
+    return null;
+  }
 
 
 
@@ -83,5 +183,6 @@ async function getEndpoints(apiBaseUrl, epicClientId) {
 
 module.exports = {
   getMetaData,
-  getEndpoints
+  getEndpoints,
+  getAuthorizationCode
 };
--- server.js
+++ server.js
@@ -18,6 +18,34 @@ app.get("/launch3", async (req, res) => {
     res.json({metadata})
 })
 
+app.get("/launch4", async (req, res) => {
+    const metadata = await epicClient.getEndpoints(req.query.iss, EPIC_CLIENT_ID);
+    const scope = "launch";
+    const state = "1234";
+
+    const authorizationCode = await epicClient.getAuthorizationCode(
+        metadata.authorizeUri,
+        EPIC_CLIENT_ID,
+        req.query.launch, 
+        req.query.iss,
+        "http://localhost:3000/callback",
+        scope,
+        state,
+        null
+      );
+
+
+    res.json({authorizationCode})
+})
+
+app.get('/callback', (req, res) => {
+    res.send('This is the callback page launched by Epic')
+  })
+
+app.get('/redirectPage', (req, res) => {
+    res.send('This is another callback page launched by Epic')
+  })
+
 
 app.listen(3000, () => {
     console.log("listening on port 3000")
